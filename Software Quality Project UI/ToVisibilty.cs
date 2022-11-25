aa6f7042a97fd6fc6a552f490116f893a0d9deeb
Braychapp <91763683+Braychapp@users.noreply.github.com>
2022-11-24 21:55:19 -0500

    Made basic UI and navigation through windows

---
+++ Software Quality Project UI/ToVisibilty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Software_Quality_Project_UI
+{
+    //public class ToVisibilty : IMultiValueConverter
+    //{
+    //    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        bool hasText = !(bool)values[0];
+    //        bool hasFocus = (bool)values[1];
+
+    //        if (hasText || hasFocus) //hiding the text visibility if it does not have text or if it has focus
+    //        {
+    //            return Visibility.Hidden;
+    //        }
+    //        else
+    //        {
+    //            return Visibility.Collapsed;
+    //        }
+    //    }
+
+    //    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
