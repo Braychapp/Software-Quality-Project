e024aa5214fa78f9489600727195d4b0057d1650
eohara97 <95063473+eohara97@users.noreply.github.com>
2022-11-25 20:15:21 -0500

    Add files via upload
    Started on Admin, Buyer, Planner class file

---
+++ Admin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace SoftwareQuality
+{
+    internal class Admin
+    {
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+
+        // Admin may access general configuration options for TMS, such as selecting directories for log files
+        private void SelectLogDirectory(string directoryPath)
+        {
+            try
+            {
+                Directory.SetCurrentDirectory(directoryPath);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Specified directory does not exist: " + e.Message);
+            }
+
+        }
+
+        // targeting IP address and ports for all DBMS communications
+        private void SetIPAddressAndPort(int newIp, int port)
+        {
+
+        }
+
+        // Admin may review logfiles without leaving the TMS application
+        private void ReviewLogFile(string filePathName)
+        {
+            try
+            {
+                string text = System.IO.File.ReadAllText(filePathName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Issue reading file: " + e.Message);
+            }
+            
+        }
+
+        // Admin may initiate a backup job on the local TMS Database, specifying the directory for the backup files to be produced
+        private void InitiateDatabaseBackup(string backupFiles)
+        {
+
+        }
+
+
+
+        private void AlterRates (decimal newRate)
+        {
+
+        }
+
+        private void AlterCarrierData()
+        {
+
+        }
+
+        private void AlterRouteTable()
+        {
+
+        }
+    }
+}
---
+++ Planner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareQuality
+{
+    internal class Planner
+    {
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+
+        // Planner receives Orders from the Buyer
+        private void ReviewOrder(string orderID)
+        {
+
+        }
+
+        // Planner selects Carriers from the targeted cities to complete the Order which adds a ‘Trip’ to the Order for each Carrier selected
+        private void SelectCarrier(string carrierID)
+        {
+
+        }
+
+        // The Planner may simulate the passage of time in 1-day increments in order to mover Orders and their trips to completed state
+        private void PassOneDay()
+        {
+
+        }
+
+        // Planner may confirm an order is completed. Completed Orders are marked for follow-up from the Buyer
+        private void ConfirmOrder(string orderID)
+        {
+
+        }
+
+        // Planner may see a summary of all active Orders in a status screen
+        private void ViewAllActiveOrders()
+        {
+
+        }
+
+        // Planner may generate a summary report of all Invoice data for the 'past 2 weeks' of simulated time
+        private void GenerateSummaryTwoWeeks()
+        {
+
+        }
+
+
+        // Planner may generate a summary report of all Invoice data for all time
+        private void GenerateSummaryAllTime()
+        {
+
+        }
+    }
+}
