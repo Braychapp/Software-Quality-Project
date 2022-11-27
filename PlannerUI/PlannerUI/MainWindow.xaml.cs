11bc1286c54c7975c9d38a8097607ebba09d2ea4
Bakr <bakr22hj@gmail.com>
2022-11-26 23:57:20 -0500

    Planner role UI pannel

---
+++ PlannerUI/PlannerUI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlannerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void received_orders_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void add_trips_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void confirm_order_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void active_orders_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void generate_summary_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
