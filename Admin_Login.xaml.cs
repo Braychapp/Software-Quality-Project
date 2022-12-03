f7d2b06e58af0d45a2022f31ad7d81f90a4c60c0
Braychapp <91763683+Braychapp@users.noreply.github.com>
2022-12-03 18:49:18 -0500

    Linked Bakr's Planner menu to the planner login

---
+++ Admin_Login.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Software_Quality_Project_UI
+{
+    /// <summary>
+    /// Interaction logic for Admin_Login.xaml
+    /// </summary>
+    public partial class Admin_Login : Window
+    {
+        public Admin_Login()
+        {
+            InitializeComponent();
+        }
+
+private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            //compare the username and their password to the username and password from the server
+            string uName = userName.Text;
+            string pass = Password.Password;
+            string test_uName = "Testing123";
+            string test_pass = "PASSWORD123";
+            string validUName = "";
+            string validPass = "";
+            string message = "";
+
+            bool goodName = false;
+            bool goodPass = false;
+
+
+
+            if (uName == test_uName) //is username is good
+            {
+                goodName = true;
+            }
+            else
+            {
+                validUName = "Incorrect Username";
+            }
+
+            if (pass == test_pass) //if password is good
+            {
+                goodPass = true;
+            }
+            else
+            {
+                validPass = "Incorrect Password";
+            }
+
+            message = validUName + "\n" + validPass;
+
+
+            if (goodName && goodPass) //if both of them are good
+            {
+                MessageBox.Show("username and pass ok");
+                //go to buyer page
+            }
+            else
+            {
+                MessageBox.Show(message);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+
+        }
+    }
+}
---
+++ Register_window.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Sql;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+
+
+namespace Software_Quality_Project_UI
+{
+    /// <summary>
+    /// Interaction logic for Register_window.xaml
+    /// </summary>
+    public partial class Register_window : Window
+    {
+        public Register_window()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //creating a new user account
+
+            Regex nameRegex = new Regex(@"^[A-z]+$");
+            Regex emailRegex = new Regex(@"^[\w -\.] +@([\w -] +\.) +[\w -]{ 2,4}+$"); //DO NOT CHANGE THIS LINE IT WORKS FOR EMAILS!
+            bool firstNameOK = false;
+            bool lastNameOK = false;
+            bool emailOK = false;
+            bool passMatch = false;
+            bool driverORuser = false;
+            
+
+            //checking the first name
+            if(nameRegex.IsMatch(firstName.Text))
+            {
+                firstNameOK = true;
+                invalidFirstName.Visibility= Visibility.Hidden;
+            }
+            else
+            {
+                invalidFirstName.Visibility = Visibility.Visible;
+                //if the name isn't in the correct format the error comes up
+                firstNameOK = false;
+            }
+
+            //checking the last name
+            if(nameRegex.IsMatch(lastName.Text))
+            {
+                lastNameOK = true;
+                invalidLastName.Visibility= Visibility.Hidden;
+            }
+            else
+            {
+                invalidLastName.Visibility = Visibility.Visible;
+                //if the last name isn't in the correct format the error shows up
+                lastNameOK = false;
+            }
+
+            //checking the email
+            if (emailRegex.IsMatch(email.Text))
+            {
+                emailOK = true;
+                invalidEmail.Visibility= Visibility.Hidden;
+            }
+            else
+            {
+                invalidEmail.Visibility = Visibility.Visible;
+                //if the email isn't in the correct format this error comes up
+                emailOK = false;
+            }
+
+            //checking if the passwords entered match
+            if (pass == confirmPass)
+            {
+                passNotMatch.Visibility = Visibility.Hidden;
+                passMatch = true;
+            }
+            else
+            {
+                passNotMatch.Visibility = Visibility.Visible;
+                passMatch = false;
+            }
+
+            string accountType = null;
+
+            if (userAccount.IsChecked == false && driverAccount.IsChecked == false)
+            {
+                //if both of these ae not checked the account can't be created the user must select one
+                invalidAccountType.Visibility= Visibility.Visible;
+                driverORuser = false;
+            }
+            else
+            {
+                driverORuser = true;
+                if(userAccount.IsChecked == true)
+                {
+                    accountType = "user";
+                    invalidAccountType.Visibility = Visibility.Hidden;
+                }
+                if(driverAccount.IsChecked == true)
+                {
+                    accountType = "driver";
+                    invalidAccountType.Visibility = Visibility.Hidden;
+                }
+            }
+
+
+            //checking if the data is all correct and if it is it will call a method to be uploaded to the SQL database
+
+            if (firstNameOK && lastNameOK && emailOK && passMatch && driverORuser)
+            {
+                //if it gets into here then all the information should be ok
+                //inside here it will submit to the SQL server
+                string data = firstName.Text + ", " + lastName.Text + ", " + email.Text + ", " + pass + ", " + "false";
+
+
+               
+                MySqlConnection conn = new MySqlConnection(); //creating a new connection
+                SQL sqlConnection = new SQL(conn);
+                sqlConnection.Register(accountType, email.Text, data);
+            }
+
+            //format the data for users and drivers like this:
+            /// Jonathon, Gregoric, asiof@gmail.com, 123abc#, false
+
+
+
+
+        }
+    }
+}
